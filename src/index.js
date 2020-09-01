5c767ce02780ea8f65068b1d7cfea8ed68a4f4f8
Shantovai23 <hasibulhasan23sept@gmail.com>
2020-09-01 21:02:46 +0600

    Multiple Jsx added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
+//reactDom.render() with single jsx
+// ReactDom.render(<h1>Hello World!</h1>,document.getElementById('root'))
 
-ReactDom.render(<h1>Hello World!</h1>,document.getElementById('root'))
+//reactDom.render() with multiple jsx
+ReactDom.render(<div>
+ <h1>Hello World!</h1>
+ <p>This Is Multiple JSX</p>
+ <a href='#'> Click Here</a>
+</div>,
+document.getElementById('root'))
