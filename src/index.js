780c5713e94ff047ce713c295aee6a124137247a
Shantovai23 <hasibulhasan23sept@gmail.com>
2020-09-01 21:58:28 +0600

    JSX expression added

--- src/index.js
+++ src/index.js
@@ -33,14 +33,27 @@ import ReactDom from 'react-dom'
 
 //React first-challenge
 
+// ReactDom.render(<React.Fragment>
+//     <h1>Netflix</h1>
+//     <p>My top 5 netflix series</p>
+//     <ul>
+//         <li>Dark</li>
+//         <li>Stranger Things</li>
+//         <li>Sex Education</li>
+//         <li>Money Heist</li>
+//         <li>Breaking Bad</li>
+//     </ul>
+// </React.Fragment>, document.getElementById('root'));
+
+
+
+
+//react jsx expression
+
+var name='Hasibul Hasan shanto'
 ReactDom.render(<React.Fragment>
-    <h1>Netflix</h1>
-    <p>My top 5 netflix series</p>
-    <ul>
-        <li>Dark</li>
-        <li>Stranger Things</li>
-        <li>Sex Education</li>
-        <li>Money Heist</li>
-        <li>Breaking Bad</li>
-    </ul>
-</React.Fragment>, document.getElementById('root'))
+ <h1>My name is {name}</h1>
+ <p>My lucky number is {12+3}</p>
+ <p>Lets see a random number {Math.floor(Math.random()*10)+1}</p>
+</React.Fragment>
+    ,document.getElementById('root'))
