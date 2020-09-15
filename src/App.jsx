7399083bb76e97f66dad5c3911a4ba58cd3cab8d
Shantovai23 <hasibulhasan23sept@gmail.com>
2020-09-15 20:52:29 +0600

    multiple input fields added

--- src/App.jsx
+++ src/App.jsx
@@ -68,37 +68,102 @@ import React, { useState } from 'react'
 
 //react froms
 
-const App=()=>{
+// const App=()=>{
  
-const [name,setName]=useState('')
-const [lastName,upLastName]=useState('')
-const [storedName,upstore]=useState('')
-const [showLastName,showUpLastName]=useState('')
+// const [name,setName]=useState('')
+// const [lastName,upLastName]=useState('')
+// const [storedName,upstore]=useState('')
+// const [showLastName,showUpLastName]=useState('')
 
-const inputEventLast=(e)=>{
-  upLastName(e.target.value)
-}
-const inputEvent=(event)=>
- setName(event.target.value)
+// const inputEventLast=(e)=>{
+//   upLastName(e.target.value)
+// }
+// const inputEvent=(event)=>
+//  setName(event.target.value)
 
- const stored=(event)=>{
-   event.preventDefault()
-   upstore(name)
-   showUpLastName(lastName)
- }
+//  const stored=(event)=>{
+//    event.preventDefault()
+//    upstore(name)
+//    showUpLastName(lastName)
+//  }
 
- return(
-  <>
-    <form onSubmit={stored}>
-    <h1>Hello {storedName} {showLastName}</h1>
-    <input type='text' placeholder='First Name' onChange={inputEvent} value={name}/>
-    <input type='text' placeholder='Last Name' onChange={inputEventLast} value={lastName}/>
-    <br/>
-    <button>Submit</button>
-    </form>
-  </>
- )
- }
+//  return(
+//   <>
+//     <form onSubmit={stored}>
+//     <h1>Hello {storedName} {showLastName}</h1>
+//     <input type='text' placeholder='First Name' onChange={inputEvent} value={name}/>
+//     <input type='text' placeholder='Last Name' onChange={inputEventLast} value={lastName}/>
+//     <br/>
+//     <button>Submit</button>
+//     </form>
+//   </>
+//  )
+//  }
 
 
+
+
+ //react multiple input fields
+
+ const App=()=>{
+ 
+  const [fullName,setFullName]=useState({
+    fName:'',
+    lName: '',
+    email:''
+  })
+
+  const inputEvent=(event)=>{
+   
+    const {value,name}=event.target
+
+    setFullName((prevalue)=>{
+      console.log(prevalue)
+
+      return{
+        ...prevalue,
+        [name]:value
+      }
+      // if(name==='fName'){
+      // return{
+      //   fName: value,
+      //   lName: prevalue.lName,
+      //   email: prevalue.email
+      // }
+      // }else if(name==='lName') {
+      //   return{
+      //     fName: prevalue.fName,
+      //     lName:value,
+      //     email: prevalue.email
+      //   }
+      // } else if(name==='email'){
+      //     return{
+      //       fName : prevalue.fName,
+      //       lName :prevalue.lName,
+      //       email: value
+      //     }
+      // } 
+    })
+
+  }
+   
+   const onSubmit=(event)=>{
+     event.preventDefault()
+     alert('Submitted')
+   }
+  
+   return(
+    <>
+      <form onSubmit={onSubmit}>
+      <h1>Hello {fullName.fName} {fullName.lName}</h1>
+      <h3>{fullName.email}</h3>
+      <input type='text' placeholder='First Name' name='fName' onChange={inputEvent} value={fullName.fName}/>
+      <input type='text' placeholder='Last Name' name='lName' onChange={inputEvent}  value={fullName.lName}/>
+      <input type='email' placeholder='Email' name='email' onChange={inputEvent}  value={fullName.email}/>
+      <br/>
+      <button>Submit</button>
+      </form>
+    </>
+   )
+   }
 export default App;
